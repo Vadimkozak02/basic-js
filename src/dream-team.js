78c503c6982381200b5d6ba17d7d01ec4bca1b93
Vadim <vadimkozak02@gmail.com>
2021-03-27 19:15:23 +0300

    complete three task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)){
+    return false;
+  }
+  let one = members.filter((word) => typeof (word) == 'string');
+  let two = one.map(word => word.trim());
+  let three = two.map(word => word[0].toUpperCase());
+  let result = three.sort().join('');
+  return result;
 };
