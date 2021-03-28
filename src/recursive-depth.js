b445db8437775f843a28404755421f204a08f1ce
Vadim <vadimkozak02@gmail.com>
2021-03-28 15:57:51 +0300

    complete task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
+  calculateDepth(arr){
+    let result = 1;
+    for (let item of arr){
+        if(Array.isArray(item)){
+            let a = 1 + this.calculateDepth(item);
+            result = (result > a) ? result : a;
+        }
+    }
+    return result;        
+    }
 };
