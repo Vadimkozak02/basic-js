3ec8b074d452146e0fe816d7566b02f6a770b5c2
Vadim <vadimkozak02@gmail.com>
2021-03-28 16:53:53 +0300

    complete

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,4 +4,22 @@ module.exports = function transform(arr) {
   if (!arr){
     throw 'Error';
   }
+  let arr_two = [];
+  for (let i = 0; i < arr.length; i++){
+    arr_two.push(arr[i]);
+    if(arr[i] == '--discard-next'){
+      arr_two.pop();
+    }
+
+    if (arr[i] == '--discard-prev'){
+      arr_two.pop();
+    }
+    if (arr[i] == '--double-next'){
+      arr_two.pop();
+    }
+    if (arr[i] == '--double-prev'){
+      arr_two.pop();
+    }
+  }
+  return arr_two;
 };
